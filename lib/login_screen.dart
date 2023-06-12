8e10e8562841d4e1d5e09904826b2b9feeb2afda
natty98282 <natty98282@gmail.com>
2023-06-12 13:21:05 +0300

    scroll view committ

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -16,63 +16,65 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: Container(
-        padding: const EdgeInsets.only(bottom: 30),
-        child: Column(
-          children: <Widget>[
-            const HeaderBar(
-              text: "LogIn",
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
-                child: Column(
-                  children: [
-                    const TextFieldItem(
-                        hint: 'Enter Your Email', icon: Icons.email),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const TextFieldItem(
-                        hint: 'Enter your Password', icon: Icons.key),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      margin: const EdgeInsets.only(top: 10),
-                      alignment: Alignment.centerRight,
-                      child: const Text('Forgot Password?'),
-                    ),
-                    const ButtonItem(
-                      buttonname: 'LogIn',
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const RegisterScreen(),
-                          ),
-                        );
-                      },
-                      child: RichText(
-                        text: TextSpan(children: [
-                          const TextSpan(
-                              text: "Don't have an account: ",
-                              style: TextStyle(color: Colors.black)),
-                          TextSpan(
-                              text: "Register",
-                              style: TextStyle(color: orangeColor)),
-                        ]),
-                      ),
-                    )
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            children: <Widget>[
+              const HeaderBar(
+                text: "LogIn",
               ),
-            )
-          ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: Column(
+                    children: [
+                      const TextFieldItem(
+                          hint: 'Enter Your Email', icon: Icons.email),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const TextFieldItem(
+                          hint: 'Enter your Password', icon: Icons.key),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        margin: const EdgeInsets.only(top: 10),
+                        alignment: Alignment.centerRight,
+                        child: const Text('Forgot Password?'),
+                      ),
+                      const ButtonItem(
+                        buttonname: 'LogIn',
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const RegisterScreen(),
+                            ),
+                          );
+                        },
+                        child: RichText(
+                          text: TextSpan(children: [
+                            const TextSpan(
+                                text: "Don't have an account: ",
+                                style: TextStyle(color: Colors.black)),
+                            TextSpan(
+                                text: "Register",
+                                style: TextStyle(color: orangeColor)),
+                          ]),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -16,67 +16,69 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: true,
-      body: Container(
-        padding: const EdgeInsets.only(bottom: 30),
-        child: Column(
-          children: <Widget>[
-            const HeaderBar(
-              text: "Register",
-            ),
-            Expanded(
-              flex: 1,
-              child: Container(
-                margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
-                child: Column(
-                  children: <Widget>[
-                    const TextFieldItem(
-                        hint: 'Enter Your Email', icon: Icons.email),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    const TextFieldItem(
-                        hint: 'Enter your Name', icon: Icons.person),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const TextFieldItem(
-                        hint: 'Enter your Phone', icon: Icons.phone),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const TextFieldItem(
-                        hint: 'Enter your Passwod', icon: Icons.key),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    const ButtonItem(
-                      buttonname: 'Register',
-                    ),
-                    InkWell(
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => const LoginScreen(),
-                          ),
-                        );
-                      },
-                      child: RichText(
-                          text: TextSpan(children: [
-                        const TextSpan(
-                            text: 'already member: ',
-                            style: TextStyle(color: Colors.black)),
-                        TextSpan(
-                            text: 'Signin',
-                            style: TextStyle(color: orangeColor))
-                      ])),
-                    )
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          padding: const EdgeInsets.only(bottom: 30),
+          child: Column(
+            children: <Widget>[
+              const HeaderBar(
+                text: "Register",
               ),
-            )
-          ],
+              Expanded(
+                flex: 1,
+                child: Container(
+                  margin: const EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: Column(
+                    children: <Widget>[
+                      const TextFieldItem(
+                          hint: 'Enter Your Email', icon: Icons.email),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      const TextFieldItem(
+                          hint: 'Enter your Name', icon: Icons.person),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextFieldItem(
+                          hint: 'Enter your Phone', icon: Icons.phone),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const TextFieldItem(
+                          hint: 'Enter your Passwod', icon: Icons.key),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const ButtonItem(
+                        buttonname: 'Register',
+                      ),
+                      InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const LoginScreen(),
+                            ),
+                          );
+                        },
+                        child: RichText(
+                            text: TextSpan(children: [
+                          const TextSpan(
+                              text: 'already member: ',
+                              style: TextStyle(color: Colors.black)),
+                          TextSpan(
+                              text: 'Signin',
+                              style: TextStyle(color: orangeColor))
+                        ])),
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
